093f8bce0d1b890bcfd43231a5f58e05c50fff42
JackVandergriff <jackvandergriff02@gmail.com>
2020-09-23 22:31:50 -0400

    Added wrapper for cpShape

--- src/chipmunk_wrapper.cpp
+++ src/chipmunk_wrapper.cpp
@@ -5,9 +5,15 @@
 #include "chipmunk_wrapper.h"
 
 #include <chipmunk/chipmunk.h>
+#include <iostream>
+#include <algorithm>
 //#include <chipmunk/chipmunk_structs.h>
 
 using namespace Bolt;
+
+cpVect cp(vec2f input) {
+    return {input.x, input.y};
+}
 
 auto RigidBody::makeRigidBody(RBTypes type) {
     switch (type) {
@@ -83,7 +89,7 @@ vec2f RigidBody::getCenterOfGravity() const {
 }
 
 void RigidBody::setCenterOfGravity(vec2f center_of_gravity) {
-    cpBodySetCenterOfGravity(body.get(), {center_of_gravity.x, center_of_gravity.y});
+    cpBodySetCenterOfGravity(body.get(), cp(center_of_gravity));
 }
 
 vec2f RigidBody::getPosition() const {
@@ -101,7 +107,7 @@ vec2f RigidBody::getVelocity() const {
 }
 
 void RigidBody::setVelocity(vec2f velocity) {
-    cpBodySetVelocity(body.get(), {velocity.x, velocity.y});
+    cpBodySetVelocity(body.get(), cp(velocity));
 }
 
 vec2f RigidBody::getForce() const {
@@ -110,7 +116,7 @@ vec2f RigidBody::getForce() const {
 }
 
 void RigidBody::setForce(vec2f force) {
-    cpBodySetForce(body.get(), {force.x, force.y});
+    cpBodySetForce(body.get(), cp(force));
 }
 
 float RigidBody::getAngularVelocity() const {
@@ -130,24 +136,24 @@ void RigidBody::setTorque(float torque) {
 }
 
 vec2f RigidBody::getVelocityAtPoint(vec2f global_point) const {
-    auto vel = cpBodyGetVelocityAtWorldPoint(body.get(), {global_point.x, global_point.y});
+    auto vel = cpBodyGetVelocityAtWorldPoint(body.get(), cp(global_point));
     return {vel.x, vel.y};
 }
 
 void RigidBody::applyForceAtPoint(vec2f force, vec2f global_point) {
-    cpBodyApplyForceAtWorldPoint(body.get(), {force.x, force.y}, {global_point.x, global_point.y});
+    cpBodyApplyForceAtWorldPoint(body.get(), cp(force), cp(global_point));
 }
 
 void RigidBody::applyForce(vec2f force) {
-    cpBodyApplyForceAtLocalPoint(body.get(), {force.x, force.y}, {0, 0});
+    cpBodyApplyForceAtLocalPoint(body.get(), cp(force), {0, 0});
 }
 
 void RigidBody::applyImpulseAtPoint(vec2f impulse, vec2f global_point) {
-    cpBodyApplyImpulseAtWorldPoint(body.get(), {impulse.x, impulse.y}, {global_point.x, global_point.y});
+    cpBodyApplyImpulseAtWorldPoint(body.get(), cp(impulse), cp(global_point));
 }
 
 void RigidBody::applyImpulse(vec2f impulse) {
-    cpBodyApplyImpulseAtLocalPoint(body.get(), {impulse.x, impulse.y}, {0, 0});
+    cpBodyApplyImpulseAtLocalPoint(body.get(), cp(impulse), {0, 0});
 }
 
 bool RigidBody::isAsleep() const {
@@ -165,3 +171,140 @@ void RigidBody::wake() {
 void RigidBody::sleep() {
     cpBodySleep(body.get());
 }
+
+PhysicsSpace &RigidBody::getSpace() const {
+    // TODO
+}
+
+PhysicsShape::PhysicsShape(RigidBody &body, float radius, vec2f offset_from_cog) : type(ShapeTypes::CIRCLE) {
+    shape = std::shared_ptr<cpShape>(cpCircleShapeNew(body.body.get(), radius, cp(offset_from_cog)), [](cpShape* s){
+        cpShapeFree(s);
+    });
+}
+
+PhysicsShape::PhysicsShape(RigidBody &body, std::pair<vec2f, vec2f> endpoints, float thickness) : type(ShapeTypes::SEGMENT) {
+    shape = std::shared_ptr<cpShape>(cpSegmentShapeNew(body.body.get(), cp(endpoints.first), cp(endpoints.second), thickness), [](cpShape* s){
+        cpShapeFree(s);
+    });
+}
+
+PhysicsShape::PhysicsShape(RigidBody &body, std::vector<vec2f> points) : type(ShapeTypes::POLYGON), polygon_vertices(points) {
+    std::vector<cpVect> cp_verts;
+    std::transform(polygon_vertices.begin(), polygon_vertices.end(), cp_verts.begin(), cp);
+    shape = std::shared_ptr<cpShape>(cpPolyShapeNew(body.body.get(), cp_verts.size(), cp_verts.data(), cpTransformIdentity, PHYSICS_BOX_RADIUS), [](cpShape* s){
+        cpShapeFree(s);
+    });
+}
+
+PhysicsShape::PhysicsShape(RigidBody &body, rectf box) : type(ShapeTypes::POLYGON) {
+    polygon_vertices = {box.getTopLeft(), box.getTopLeft() + vec2f{box.w, 0}, box.getBottomRight(), box.getTopLeft() + vec2f{0, box.h}};
+    shape = std::shared_ptr<cpShape>(cpBoxShapeNew2(body.body.get(), cpBBNew(box.x, box.y, box.x + box.w, box.y + box.h), PHYSICS_BOX_RADIUS), [](cpShape* s){
+        cpShapeFree(s);
+    });
+}
+
+ShapeTypes PhysicsShape::getType() const {
+    return type;
+}
+
+RigidBody &PhysicsShape::getBody() const {
+    // TODO
+}
+
+void PhysicsShape::setBody(RigidBody &body) {
+    cpShapeSetBody(shape.get(), body.body.get());
+}
+
+bool PhysicsShape::isSensor() const {
+    return cpShapeGetSensor(shape.get());
+}
+
+void PhysicsShape::setSensor(bool is_sensor) {
+    cpShapeSetSensor(shape.get(), is_sensor);
+}
+
+float PhysicsShape::getElasticity() const {
+    return cpShapeGetElasticity(shape.get());
+}
+
+void PhysicsShape::setElasticity(float elasticity) {
+    cpShapeSetElasticity(shape.get(), elasticity);
+}
+
+float PhysicsShape::getFriction() const {
+    return cpShapeGetFriction(shape.get());
+}
+
+void PhysicsShape::setFriction(float friction) {
+    cpShapeSetFriction(shape.get(), friction);
+}
+
+vec2f PhysicsShape::getSurfaceVelocity() const {
+    auto vel = cpShapeGetSurfaceVelocity(shape.get());
+    return {vel.x, vel.y};
+}
+
+void PhysicsShape::setSurfaceVelocity(vec2f velocity) {
+    cpShapeSetSurfaceVelocity(shape.get(), cp(velocity));
+}
+
+CollisionFilter PhysicsShape::getCollisionFilter() const {
+    // TODO
+    return {};
+}
+
+void PhysicsShape::setCollisionFilter(CollisionFilter filter) {
+    // TODO
+}
+
+PhysicsSpace &PhysicsShape::getSpace() const {
+    // TODO
+}
+
+vec2f PhysicsShape::circleGetOffset() const {
+    if (type != ShapeTypes::CIRCLE) throw "This ain't a circle nor a good error message";
+    auto off = cpCircleShapeGetOffset(shape.get());
+    return {off.x, off.y};
+}
+
+float PhysicsShape::circleGetRadius() const {
+    if (type != ShapeTypes::CIRCLE) throw "This ain't a circle nor a good error message";
+    return cpCircleShapeGetRadius(shape.get());
+}
+
+std::tuple<vec2f, vec2f> PhysicsShape::segmentGetEndpoints() const {
+    if (type != ShapeTypes::SEGMENT) throw "This ain't a segment nor a good error message";
+    auto A = cpSegmentShapeGetA(shape.get());
+    auto B = cpSegmentShapeGetB(shape.get());
+    return {{A.x, A.y}, {B.x, B.y}};
+}
+
+vec2f PhysicsShape::segmentGetNormalVector() const {
+    if (type != ShapeTypes::SEGMENT) throw "This ain't a segment nor a good error message";
+    auto norm = cpSegmentShapeGetNormal(shape.get());
+    return {norm.x, norm.y};
+}
+
+float PhysicsShape::segmentGetThickness() const {
+    if (type != ShapeTypes::SEGMENT) throw "This ain't a segment nor a good error message";
+    return cpSegmentShapeGetRadius(shape.get());
+}
+
+void PhysicsShape::segmentSetNeighborPoint(vec2f previous, vec2f next) {
+    if (type != ShapeTypes::SEGMENT) throw "This ain't a segment nor a good error message";
+    cpSegmentShapeSetNeighbors(shape.get(), cp(previous), cp(next));
+}
+
+const std::vector<vec2f> &PhysicsShape::polygonGetPoints() const {
+    if (type != ShapeTypes::POLYGON) throw "This ain't a polygon nor a good error message";
+    return polygon_vertices;
+}
+
+vec2f PhysicsShape::polygonGetCentroid() const {
+    std::vector<cpVect> cp_verts;
+    std::transform(polygon_vertices.begin(), polygon_vertices.end(), cp_verts.begin(), cp);
+    auto cent = cpCentroidForPoly(cp_verts.size(), cp_verts.data());
+    return {cent.x, cent.y};
+}
+
+
--- src/chipmunk_wrapper.h
+++ src/chipmunk_wrapper.h
@@ -6,32 +6,46 @@
 #define BOLT_CHIPMUNK_WRAPPER_H
 
 #include <memory>
+#include <utility>
+#include <vector>
 
 #include "transform.h"
 #include "utility.h"
 
 class cpBody;
+class cpShape;
 
 namespace Bolt {
+
+    const static inline float PHYSICS_BOX_RADIUS = 0.1f;
 
     enum class RBTypes {
         DYNAMIC, KINEMATIC, STATIC
     };
 
+    enum class ShapeTypes {
+        CIRCLE, SEGMENT, POLYGON
+    };
+
     class PhysicsSpace;
+
+    struct CollisionFilter {
+        // TODO
+    };
 
     class RigidBody : Component {
     private:
         std::shared_ptr<cpBody> body;
         auto makeRigidBody(RBTypes type);
     public:
+        friend class PhysicsShape;
         RigidBody();
         RigidBody(RBTypes type);
         void onUpdate() override;
 
         RBTypes getType() const;
         void setType(RBTypes type);
-        const PhysicsSpace& getSpace() const;
+        PhysicsSpace& getSpace() const; // TODO
         float getMass() const;
         void setMass(float mass);
         float getMoment() const;
@@ -58,6 +72,49 @@ namespace Bolt {
         bool isAsleep() const;
         void wake();
         void sleep();
+    };
+
+    class PhysicsShape {
+    private:
+        std::shared_ptr<cpShape> shape;
+        const ShapeTypes type;
+        std::vector<vec2f> polygon_vertices;
+    public:
+        PhysicsShape() = delete; // It's either this or breaking if someone tries to use it uninitialized
+        PhysicsShape(RigidBody& body, float radius, vec2f offset_from_cog); // Circle constructor
+        PhysicsShape(RigidBody& body, std::pair<vec2f, vec2f> endpoints, float thickness); // Segment constructor
+        PhysicsShape(RigidBody& body, std::vector<vec2f> points); // Polygon constructor
+        PhysicsShape(RigidBody& body, rectf box); // Box constructor
+
+        ShapeTypes getType() const;
+        RigidBody& getBody() const; // TODO
+        void setBody(RigidBody& body);
+        bool isSensor() const;
+        void setSensor(bool is_sensor);
+        float getElasticity() const;
+        void setElasticity(float elasticity);
+        float getFriction() const;
+        void setFriction(float friction);
+        vec2f getSurfaceVelocity() const;
+        void setSurfaceVelocity(vec2f velocity);
+        CollisionFilter getCollisionFilter() const; // TODO
+        void setCollisionFilter(CollisionFilter filter); // TODO
+        PhysicsSpace& getSpace() const; // TODO
+
+        // Circle methods
+        vec2f circleGetOffset() const;
+        float circleGetRadius() const;
+
+        // Segment methods
+        std::tuple<vec2f, vec2f> segmentGetEndpoints() const;
+        vec2f segmentGetNormalVector() const;
+        float segmentGetThickness() const;
+        void segmentSetNeighborPoint(vec2f previous, vec2f next);
+
+        // Polygon methods
+        const std::vector<vec2f>& polygonGetPoints() const;
+        // float polygonGetScale() const; Pretty sure this isn't really what radius is, needs testing TODO
+        vec2f polygonGetCentroid() const;
     };
 
     class PhysicsSpace {
